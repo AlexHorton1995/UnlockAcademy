58087e700140ad838aad479d104cfc6dffcf5bfa
Alex Horton <alex.d.horton95@gmail.com>
2020-04-12 15:11:48 -0500

    made changes to Lesson5.py.  Added updated function call, removed redundant code in while loop.

--- Python/Lesson5.py
+++ Python/Lesson5.py
@@ -1,39 +1,90 @@
 ############################
 #   Lesson 5 - Custom Functions
-#
-#
-#
-#
-#
-#
+#   Author: alexhortdog95
+#   Date:  04/12/2020
+#   Purpose:  Learn how to create custom functions in Python code.
 #############################
 
 expected_salaries = {"NY": 70000, "CA": 70000, "FL": 50000, "NC": 50000, "TX": 60000}  # this is a dictionary
 valid_states = ("FL", "NY", "CA", "TX", "NC")  # this is a tuple
 is_Still_Looping = True
+new_expected_salaries = 0
 user_lists = []
 
 
 def calculate_salary(userinfo, exp, state, langs, edyears):
-    
     state_sals = expected_salaries[state]
     new_expected_salaries = state_sals - 5000    # 70,000 - 5,000 = $65,000        
 
-    if len(langs) < 3:
-        new_expected_salaries = new_expected_salaries - 10000   # 65,000 - 10,000 = $55,000
-        print("learn some more coding languages; deduct $10k from expected salary.")
-    elif len(langs) > 3:
-        new_expected_salaries = new_expected_salaries + 10000
-    else:
-        new_expected_salaries = new_expected_salaries + 5000         
-            
-    if int(edyears) > 3:
-        new_expected_salaries = new_expected_salaries + 5000
-    else:
-        new_expected_salaries = new_expected_salaries - 5000        
+    if exp == 1:
+        new_expected_salaries = state_sals - 5000
 
-    print('Expect $' + str(new_expected_salaries) + " for your level of experience")
+        if len(langs) < 3:
+            new_expected_salaries = new_expected_salaries - 10000   # 65,000 - 10,000 = $55,000
+            print("learn some more coding languages; deduct $10k from expected salary.")
+        elif len(langs) > 3:
+            new_expected_salaries = new_expected_salaries + 10000
+        else:
+            new_expected_salaries = new_expected_salaries + 5000         
+                
+        if int(edyears) > 3:
+            new_expected_salaries = new_expected_salaries + 5000
+        else:
+            new_expected_salaries = new_expected_salaries - 5000        
+
+    elif exp == 2:
+            new_expected_salaries = state_sals - 5000        
             
+            if len(user_coding_languages) < 3:
+                new_expected_salaries = new_expected_salaries - 10000
+                print("learn some more coding languages; deduct $10k from expected salary")
+            elif len(user_coding_languages) > 3:
+                new_expected_salaries = new_expected_salaries + 10000
+            else:
+                new_expected_salaries = new_expected_salaries + 5000    
+            
+            if int(number_of_education_years) > 3:
+                new_expected_salaries = new_expected_salaries + 5000
+            else:
+                new_expected_salaries = new_expected_salaries - 5000        
+                print("Expect $" + str(new_expected_salaries) + " for your level of experience")
+
+    elif exp == 3:
+            new_expected_salaries = state_sals - 5000    
+
+            if len(user_coding_languages) < 3:
+                new_expected_salaries = new_expected_salaries - 10000
+                print("learn some more coding languages; deduct $10k from expected salary")
+            elif len(user_coding_languages) > 3:
+                new_expected_salaries = new_expected_salaries + 10000
+            else:
+                new_expected_salaries = new_expected_salaries + 5000    
+            
+            if int(number_of_education_years) > 3:
+                new_expected_salaries = new_expected_salaries + 5000
+            else:
+                new_expected_salaries = new_expected_salaries - 5000        
+                print("Expect $" + str(new_expected_salaries) + " for your level of experience")
+
+    elif exp == 4:
+            new_expected_salaries = state_sals - 5000    
+        
+            if len(user_coding_languages) < 3:
+                new_expected_salaries = new_expected_salaries - 10000
+                print("learn some more coding languages; deduct $10k from expected salary")
+            elif len(user_coding_languages) > 3:
+                new_expected_salaries = new_expected_salaries + 10000
+            else:
+                new_expected_salaries = new_expected_salaries + 5000    
+                
+            if int(number_of_education_years) > 3:
+                new_expected_salaries = new_expected_salaries + 5000
+            else:
+                new_expected_salaries = new_expected_salaries - 5000        
+                print("Expect $" + str(new_expected_salaries) + " for your level of experience")
+
+    return new_expected_salaries
+    
 
 while is_Still_Looping:
     try:
@@ -41,11 +92,8 @@ while is_Still_Looping:
         user_dob = input("Please enter your Date of Birth (MM/DD/YYYY): \n")
         user_country = input("Enter Country:(Use Country Code ex.USA): \n")
 
-        print(valid_states[0])
-        print(valid_states[1])
-        print(valid_states[2])
-        print(valid_states[3])
-        print(valid_states[4])
+        for states in valid_states:
+            print(states)
 
         state_salaries = 0
 
@@ -64,82 +112,8 @@ while is_Still_Looping:
 
         user_info = {"DOB": user_dob, "Fullname": user_full_name, "Country": user_country, "State": user_state,
                     "is_active": user_is_active, "number_of_education_years": number_of_education_years, "Adjusted_Salary": state_salaries}
-                    
-        if user_experience == 1:  # if user_experience chosen is 1
-            calculate_salary(user_info, user_experience, user_state, user_coding_languages, number_of_education_years)
 
-            state_salaries = expected_salaries[user_info["State"]]
-            new_expected_salaries = state_salaries - 5000    # 70,000 - 5,000 = $65,000        
-
-            if len(user_coding_languages) < 3:
-                new_expected_salaries = new_expected_salaries - 10000   # 65,000 - 10,000 = $55,000
-                print("learn some more coding languages; deduct $10k from expected salary.")
-            elif len(user_coding_languages) > 3:
-                new_expected_salaries = new_expected_salaries + 10000
-            else:
-                new_expected_salaries = new_expected_salaries + 5000         
-            
-            if int(number_of_education_years) > 3:
-                new_expected_salaries = new_expected_salaries + 5000
-            else:
-                new_expected_salaries = new_expected_salaries - 5000        
-                print('Expect $' + str(new_expected_salaries) + " for your level of experience")
-            
-        elif user_experience == 2:  # if user_experience chosen is 2
-            state_salaries = expected_salaries[user_info["State"]]
-            new_expected_salaries = state_salaries - 5000        
-            
-            if len(user_coding_languages) < 3:
-                new_expected_salaries = new_expected_salaries - 10000
-                print("learn some more coding languages; deduct $10k from expected salary")
-            elif len(user_coding_languages) > 3:
-                new_expected_salaries = new_expected_salaries + 10000
-            else:
-                new_expected_salaries = new_expected_salaries + 5000    
-            
-            if int(number_of_education_years) > 3:
-                new_expected_salaries = new_expected_salaries + 5000
-            else:
-                new_expected_salaries = new_expected_salaries - 5000        
-                print("Expect $" + str(new_expected_salaries) + " for your level of experience")
-            
-        elif user_experience == 3:  # if user_experience chosen is 3
-            state_salaries = expected_salaries[user_info["State"]]
-            new_expected_salaries = state_salaries - 5000    
-
-            if len(user_coding_languages) < 3:
-                new_expected_salaries = new_expected_salaries - 10000
-                print("learn some more coding languages; deduct $10k from expected salary")
-            elif len(user_coding_languages) > 3:
-                new_expected_salaries = new_expected_salaries + 10000
-            else:
-                new_expected_salaries = new_expected_salaries + 5000    
-            
-            if int(number_of_education_years) > 3:
-                new_expected_salaries = new_expected_salaries + 5000
-            else:
-                new_expected_salaries = new_expected_salaries - 5000        
-                print("Expect $" + str(new_expected_salaries) + " for your level of experience")
-                
-        elif user_experience == 4:  # if user_experience chosen is 4
-            state_salaries = expected_salaries[user_info["State"]]
-            new_expected_salaries = state_salaries - 5000    
-        
-            if len(user_coding_languages) < 3:
-                new_expected_salaries = new_expected_salaries - 10000
-                print("learn some more coding languages; deduct $10k from expected salary")
-            elif len(user_coding_languages) > 3:
-                new_expected_salaries = new_expected_salaries + 10000
-            else:
-                new_expected_salaries = new_expected_salaries + 5000    
-                
-            if int(number_of_education_years) > 3:
-                new_expected_salaries = new_expected_salaries + 5000
-            else:
-                new_expected_salaries = new_expected_salaries - 5000        
-                print("Expect $" + str(new_expected_salaries) + " for your level of experience")
-
-        user_info["Adjusted_Salary"] = new_expected_salaries
+        user_info["Adjusted_Salary"] = calculate_salary(user_info, user_experience, user_state, user_coding_languages, number_of_education_years)
 
         if len(user_full_name) < 5:
             print("Error:Complete Full Name")
