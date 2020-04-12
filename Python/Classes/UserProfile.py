7182222fb0eac9e9fea54600be8572eb51368d44
Alex Horton <alex.d.horton95@gmail.com>
2020-04-12 15:58:15 -0500

    Added Prettier for file cleanliness

--- Python/Classes/UserProfile.py
+++ Python/Classes/UserProfile.py
@@ -1,19 +1,33 @@
-#UserProfile.py
-'''
+# UserProfile.py
+"""
         user_info = {"DOB": user_dob, "Fullname": user_full_name, "Country": user_country, "State": user_state,
                     "is_active": user_is_active, "number_of_education_years": number_of_education_years, "Adjusted_Salary": state_salaries}
 
-'''
+"""
 
 
 class UserProfile:
-    #constructor
-    def __init__(self, pPass, pName, pAdd1, pAdd2, pCity, pState, pZip, pEmail, pDOB, pCountry, pStatus, pEdYears):
-        #use super().__init__() when you are inheriting from a base class.
-        #PCI Info
+    # constructor
+    def __init__(
+        self,
+        pPass,
+        pName,
+        pAdd1,
+        pAdd2,
+        pCity,
+        pState,
+        pZip,
+        pEmail,
+        pDOB,
+        pCountry,
+        pStatus,
+        pEdYears,
+    ):
+        # use super().__init__() when you are inheriting from a base class.
+        # PCI Info
         self.Password = pPass
 
-        #PII Info
+        # PII Info
         self.FullName = pName
         self.Address1 = pAdd1
         self.Address2 = pAdd2
@@ -34,6 +48,3 @@ class UserProfile:
 
     def addStuff(self):
         pass
-
-
-    
