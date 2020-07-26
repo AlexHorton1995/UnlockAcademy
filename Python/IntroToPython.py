f78fabf4dc22628bc18250f9d4caf3cdaca2689f
alexhorton1995 <alex.d.horton95@gmail.com>
2020-07-26 00:57:50 -0500

    adding output.txt and input .txt for lesson in Pluralsight

---
+++ Python/IntroToPython.py
@@ -0,0 +1,11 @@
+import json
+
+### with open() as something is basically same as using in c#
+with open("input.json", "r") as input:
+    obj = json.load(input)
+
+    with open("output.txt", "w") as output:
+        output.write(obj["name"] + "'s Hobbies:\n")
+        for hobby in obj["hobbies"]:
+            output.write(hobby + "\n")
+
