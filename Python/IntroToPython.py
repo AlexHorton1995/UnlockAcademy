56d30eed8da3b256a562db715fee4eab32141128
Alex Horton <alex.d.horton95@gmail.com>
2020-07-28 02:56:38 -0500

    Changing IntroToPython.py code so that it works with Linux based system.

--- Python/IntroToPython.py
+++ Python/IntroToPython.py
@@ -1,10 +1,13 @@
 import json
 
-### with open() as something is basically same as using in c#
-with open("input.json", "r") as input:
-    obj = json.load(input)
+##Relative file paths of inpput and output files.
+inputPath = "/home/alex/UnlockAcademy/Python/input.json"
+outputPath = "/home/alex/UnlockAcademy/Python/output.txt" 
 
-    with open("output.txt", "w") as output:
+### with open() as something is basically same as using in c#
+with open(inputPath, "r") as input:
+    obj = json.load(input)
+    with open(outputPath, "w") as output:
         output.write(obj["name"] + "'s Hobbies:\n")
         for hobby in obj["hobbies"]:
             output.write(hobby + "\n")
