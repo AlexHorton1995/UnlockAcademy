aae4d6a6862526550a71f2cf1684a02a772af4d5
AlexHorton1995 <alex.d.horton95@gmail.com>
2020-08-07 23:30:34 -0500

    Adding unlockreact folder to source

--- unlockreact/src/index.js
+++ unlockreact/src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function MyInfo() {
+  return (
+    <div>
+      <p></p>
+      <h1>Alex Horton</h1>
+      <p>
+        <p>Top Three Vacation Spots to Visit are</p>
+        <ol>
+          <li>Hawaii</li>
+          <li>Japan</li>
+          <li>France</li>
+        </ol>
+      </p>
+    </div>)
+}
+
+ReactDOM.render( 
+  <MyInfo />
+  , document.getElementById("root")
+)
